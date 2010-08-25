ff8750b4dc65f96e3c17381b302ef10cd9065125
Eugeniy Meshcheryakov <eugen@debian.org>
2010-08-25 22:21:45 +0200

    Add setup script and empty MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = 'python-gdsii',
+    version = '0.0',
+    description = 'GDSII manipulation libaray',
+    author = 'Eugeniy Meshcheryakov',
+    author_email = 'eugen@debian.org',
+    url = 'http://www.gitorious.org/python-gdsii',
+    packages = ['gdsii'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    license = 'LGPL-3+'
+)
