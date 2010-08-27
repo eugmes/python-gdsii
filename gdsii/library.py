fbd02b68dbc9934190f182ae34c3ec0218ea1ae7
Eugeniy Meshcheryakov <eugen@debian.org>
2010-08-27 20:49:25 +0200

    Use gdsii._record for reading and writing

--- gdsii/library.py
+++ gdsii/library.py
@@ -19,119 +19,66 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 from __future__ import absolute_import
-from . import tags, _utils, FormatError, _ignore_record, RecordData
+from . import tags, _utils, _records, FormatError, _ignore_record, RecordData
 from .structure import Structure
 
+header = _records.SimpleRecord('version', tags.HEADER,
+""" GDSII file verion.
+
+    Integer number as found in a GDSII file. For example value is 5 for GDSII v5
+    and 0x600 for GDSII v6.
+""")
+bgnlib = _records.TimestampsRecord('mod_time', 'acc_time', tags.BGNLIB,
+    'Last modification time (datetime).', 'Last access time (datetime).')
+libdirsize = _records.SimpleOptionalRecord('libdirsize', tags.LIBDIRSIZE, None)
+srfname = _records.OptionalWholeRecord('srfname', tags.SRFNAME, 'Name of spacing rules file (bytes, optional).')
+libsecur = _records.ACLRecord('acls', tags.LIBSECUR, 'ACL data, list of tuples (GID, UID, ACCESS).')
+libname = _records.StringRecord('name', tags.LIBNAME, 'Library name (bytes).')
+reflibs = _records.OptionalWholeRecord('reflibs', tags.REFLIBS, None)
+fonts = _records.OptionalWholeRecord('fonts', tags.FONTS, None)
+attrtable = _records.OptionalWholeRecord('attrtable', tags.ATTRTABLE, None)
+generations = _records.SimpleOptionalRecord('generations', tags.GENERATIONS, None)
+format = _records.FormatRecord('format', 'masks', tags.FORMAT, None, None)
+units = _records.UnitsRecord('logical_unit', 'physical_unit', tags.UNITS, None, None)
+
+@_records.stream_class
 class Library(object):
-    """GDSII library class."""
-    __slots__ = ['_version', '_mod_time', '_acc_time', '_name', '_logical_unit', '_physical_unit', '_structures']
+    """GDSII library class.
+
+    GDS Syntax:
+
+          HEADER BGNLIB [LIBDIRSIZE] [SRFNAME] [LIBSECUR] LIBNAME [REFLIBS]
+          [FONTS] [ATTRTABLE] [GENERATIONS] [<format>] UNITS {<structure>}* ENDLIB
+    """
+    _gds_objs = (header, bgnlib, libdirsize, srfname, libsecur, libname, reflibs,
+            fonts, attrtable, generations, format, units)
 
     def __init__(self, unbuf_recs):
         recs = _utils.BufferedIterator(unbuf_recs)
         self._structures = []
 
-        rec = next(recs)
-        # HEADER
-        rec.check_tag(tags.HEADER)
-        rec.check_size(1)
-        self._version = rec.data[0]
-
-        # BGNLIB
-        rec = next(recs)
-        rec.check_tag(tags.BGNLIB)
-        self._mod_time, self._acc_time = rec.times
-
-        # ignore posssible LIBDIRSIZE, SRFNAME, LIBSECUR
-        rec = _ignore_record(recs, next(recs), tags.LIBDIRSIZE)
-        rec = _ignore_record(recs, rec, tags.SRFNAME)
-        rec = _ignore_record(recs, rec, tags.LIBSECUR)
-
-        # LIBNAME
-        rec.check_tag(tags.LIBNAME)
-        self._name = rec.data
-
-        # ignore posssible REFLIBS, FONTS, ATTRTABLE, GENERATIONS
-        rec = _ignore_record(recs, next(recs), tags.REFLIBS)
-        rec = _ignore_record(recs, rec, tags.FONTS)
-        rec = _ignore_record(recs, rec, tags.ATTRTABLE)
-        rec = _ignore_record(recs, rec, tags.GENERATIONS)
-
-        # ignore FORMAT and following MASK+ ENDMASKS
-        if rec.tag == tags.FORMAT:
-            rec = next(recs)
-            if rec.tag == tags.MASK:
-                while True:
-                    rec = next(recs)
-                    if rec.tag == tags.ENDMASKS:
-                        rec = next(recs)
-                        break
-                    rec.check_tag(tags.MASK)
-
-        # UNITS
-        rec.check_tag(tags.UNITS)
-        rec.check_size(2)
-        self._logical_unit, self._physical_unit = rec.data
+        next(recs)
+        for obj in self._gds_objs:
+            obj.read(self, recs)
 
         # read structures starting with BGNSTR or ENDLIB
+        rec = recs.current
         while True:
-            rec = next(recs)
             if rec.tag == tags.BGNSTR:
                 self._structures.append(Structure(recs))
+                rec = next(recs)
             elif rec.tag == tags.ENDLIB:
                 break
             else:
-                raise FormatError('unexpected tag where BGNSTR or ENDLIB are expected')
+                raise FormatError('unexpected tag where BGNSTR or ENDLIB are expected: %d', rec.tag)
 
     def save(self, stream):
-        RecordData(tags.HEADER, (self._version,)).save(stream)
-        RecordData(tags.BGNLIB, times=(self._mod_time, self._acc_time)).save(stream)
-        # ignore tags.LIBDIRSIZE
-        # ignore tags.SRFNAME
-        # ignore tags.LIBSECUR
-        RecordData(tags.LIBNAME, self._name).save(stream)
-        # ignore tags.REFLIBS
-        # ignore tags.FONTS
-        # ignore tags.ATTRTABLE
-        # ignore tags.GENERATIONS
-        # FORMAT and following MASK+ ENDMASKS
-        RecordData(tags.UNITS, (self._logical_unit, self._physical_unit)).save(stream)
+        for obj in self._gds_objs:
+            obj.save(self, stream)
         for struc in self._structures:
             struc.save(stream)
         RecordData(tags.ENDLIB).save(stream)
 
-    @property
-    def version(self):
-        """GDSII file verion.
-
-        Integer number as found in a GDSII file. For example value is 5 for GDSII v5
-        and 0x600 for GDSII v6.
-        """
-        return self._version
-
-    @property
-    def mod_time(self):
-        """Last modification time (datetime)."""
-        return self._mod_time
-
-    @property
-    def acc_time(self):
-        """Last access time (datetime)."""
-        return self._acc_time
-
-    @property
-    def name(self):
-        """Name of the library (bytes)."""
-        return self._name
-
-    @property
-    def logical_unit(self):
-        return self._logical_unit
-
-    @property
-    def physical_unit(self):
-        return self._physical_unit
-
-    # TODO remove
     @property
     def structures(self):
         """List of structures in this library (:class:`pygdsii.structure.Structure`)."""
